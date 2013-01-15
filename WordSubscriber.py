9a7f97ebd377c3a346823aa826543d1a991a8367
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2013-01-15 02:39:45 +0100

    Add main function.

--- WordSubscriber.py
+++ WordSubscriber.py
@@ -22,6 +22,19 @@ DB_FILE = os.path.join(HERE, 'SubscriberDB.txt')
 # subreddit, then we will message for every returned result.
 IDS_KEPT = 3
 
+def main():
+    r = praw.Reddit('WordSubscriber by u/_Daimon_ ver 0.1.1. Source see '
+                    'github.com/Damgaard/Reddit-Bots')
+    r.login(USERNAME, PASSWORD)
+    last_found_ids = load_last_found_ids()
+    search_word = 'praw'
+    bad_words = ['subreddit stats', 'electro', 'dubstep', 'house', 'music']
+    newest_ids = get_newest_ids(r, search_word)
+    for result in search_results(r, search_word, last_found_ids):
+        if is_valid_result(result, bad_words):
+            message_me(r, result, search_word)
+    store_last_found_ids(newest_ids)
+
 def load_last_found_ids():
     try:
         with open(DB_FILE, 'r') as db:
@@ -36,20 +49,20 @@ def store_last_found_ids(new_id):
         for id in new_id:
             db.write("%s\n" % id)
 
-def search_results(search_word, stop_ids):
+def search_results(reddit_session, search_word, stop_ids):
     """
     Yield search results coming from the search_word.
 
     Stop when we hit stop_id
 
     """
-    for result in r.search(search_word, sort='new', limit=None,
-                           place_holder=stop_ids[0]):
+    for result in reddit_session.search(search_word, sort='new', limit=None,
+                                        place_holder=stop_ids[0]):
         if result.id in stop_ids:
             break
         yield result
 
-def get_newest_ids(search_word):
+def get_newest_ids(reddit_session, search_word):
     result_generator = reddit_session.search(search_word, limit=IDS_KEPT)
     newest_ids = list(result.id for result in result_generator)
     return newest_ids + [None] * (IDS_KEPT - len(newest_ids))
@@ -61,19 +74,10 @@ def is_valid_result(result, bad_words):
                                   for word in bad_words)
     return good_title and good_subreddit
 
-def message_me(result, search_word):
+def message_me(reddit_session, result, search_word):
     title = 'New post about %s' % search_word
     body =  '[%s](%s)' % (result.title, result.url)
-    r.send_message('_Daimon_', title, body)
+    reddit_session.send_message('_Daimon_', title, body)
 
-r = praw.Reddit('WordSubscriber by u/_Daimon_ ver 0.1.1. Source see '
-                'github.com/Damgaard/Reddit-Bots')
-r.login(USERNAME, PASSWORD)
-last_found_ids = load_last_found_ids()
-search_word = 'praw'
-bad_words = ['subreddit stats', 'electro', 'dubstep', 'house', 'music']
-newest_ids = get_newest_ids(search_word)
-for result in search_results(search_word, last_found_ids):
-    if is_valid_result(result, bad_words):
-        message_me(result, search_word)
-store_last_found_ids(newest_ids)
+if __name__ == '__main__':
+    main()
