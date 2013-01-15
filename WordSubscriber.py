e8f8e96b7a916e68f0b31ded63dacf5a6a400e17
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2013-01-15 02:12:18 +0100

    Use multiple ids instead of one.
    This makes the program more resilliant and capable of surviving
    the place_holder id being deleted.

--- WordSubscriber.py
+++ WordSubscriber.py
@@ -17,19 +17,26 @@ from authentication import USERNAME, PASSWORD
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 DB_FILE = os.path.join(HERE, 'SubscriberDB.txt')
+# The number of id's we keep to make sure that a result haven't been processed
+# before. One is insufficient as if becomes deleted/removed/placed in private
+# subreddit, then we will message for every returned result.
+IDS_KEPT = 3
 
-def load_last_found_id():
+def load_last_found_ids():
     try:
         with open(DB_FILE, 'r') as db:
-            return db.read().strip()
+            last_ids = db.readlines()[:IDS_KEPT]
+            last_ids = [id.strip() for id in last_ids if id.strip() != '']
+            return last_ids + [None] * (IDS_KEPT - len(last_ids))
     except IOError:
-        return None
+        return [None] * IDS_KEPT
 
-def store_last_found_id(new_id):
+def store_last_found_ids(new_id):
     with open(DB_FILE, 'w') as db:
-        db.write(new_id)
+        for id in new_id:
+            db.write("%s\n" % id)
 
-def search_results(search_word, stop_id):
+def search_results(search_word, stop_ids):
     """
     Yield search results coming from the search_word.
 
@@ -37,11 +44,15 @@ def search_results(search_word, stop_id):
 
     """
     for result in r.search(search_word, sort='new', limit=None,
-                           place_holder=stop_id):
+                           place_holder=stop_ids[0]):
+        if result.id in stop_ids:
+            break
         yield result
 
-def get_newest_id(search_word):
-    return search_results(search_word, None).next().id
+def get_newest_ids(search_word):
+    result_generator = reddit_session.search(search_word, limit=IDS_KEPT)
+    newest_ids = list(result.id for result in result_generator)
+    return newest_ids + [None] * (IDS_KEPT - len(newest_ids))
 
 def is_valid_result(result, bad_words):
     """Does the title or subreddit contain a bad word?"""
@@ -58,11 +69,11 @@ def message_me(result, search_word):
 r = praw.Reddit('WordSubscriber by u/_Daimon_ ver 0.1.1. Source see '
                 'github.com/Damgaard/Reddit-Bots')
 r.login(USERNAME, PASSWORD)
-last_found_id = load_last_found_id()
+last_found_ids = load_last_found_ids()
 search_word = 'praw'
 bad_words = ['subreddit stats', 'electro', 'dubstep', 'house', 'music']
-newest_id = get_newest_id(search_word)
-for result in search_results(search_word, last_found_id):
+newest_ids = get_newest_ids(search_word)
+for result in search_results(search_word, last_found_ids):
     if is_valid_result(result, bad_words):
         message_me(result, search_word)
-store_last_found_id(newest_id)
+store_last_found_ids(newest_ids)
